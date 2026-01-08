83864b7ec3f3539a522a0897c05834349219d179
tamannabhatti0516 <tamannabhatti0516@gmail.com>
2026-01-08 16:13:11 +0000

    fixed ui

--- app.py
+++ app.py
@@ -54,5 +54,5 @@ def predict():
     )
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5001))
     flask_app.run(host='0.0.0.0', port=port)
